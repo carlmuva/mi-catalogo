4d9e334b774b4cf3000f16575e98089329081c76
carlmuva <carlmuva@hotmail.com>
2020-06-13 15:51:42 -0500

    E3 A#6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { TableComponent } from './table/table.component';
+import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
 
 
 
 const routes: Routes = [
+{path:'', redirectTo:'/list', pathMatch:'full'},
 {
     path: 'list',
     component: ListComponent
@@ -13,6 +15,10 @@ const routes: Routes = [
 {
     path: 'table',
     component: TableComponent
+},
+{
+  path:'**',
+  component:PageNotFoundComponentComponent
 }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { VistasComponent } from './vistas/vistas.component';
 import { ListComponent } from './list/list.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TableComponent } from './table/table.component';
+import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { TableComponent } from './table/table.component';
     HeaderComponent,
     VistasComponent,
     ListComponent,
-    TableComponent
+    TableComponent,
+    PageNotFoundComponentComponent
   ],
   imports: [
     BrowserModule,
